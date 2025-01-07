ae1a5e6a80e77ed7dbf9eed5c0f608699fc53dc5
Sharon Xie <rxie@decodable.co>
2025-01-06 21:42:09 -0800

    wip

--- .github/plugin-discovery/plugin_discovery.py
+++ .github/plugin-discovery/plugin_discovery.py
@@ -5,14 +5,14 @@ import sys
 
 def main():
     out = subprocess.run(["dbt", "--version"], stderr=subprocess.PIPE, text=True)
-    print(out.stdout)
+    print(out.stderr)
     plugin_detected = (
-        re.search(r"Plugins:.*\s- decodable: \d+\.\d+\.\d+", out.stdout, flags=re.DOTALL)
+        re.search(r"Plugins:.*\s- decodable: \d+\.\d+\.\d+", out.stderr, flags=re.DOTALL)
         is not None
     )
     if not plugin_detected:
         sys.exit(
-            f"Decodable plugin not recognized by dbt! Received output of `dbt --version`:\n{out.stdout}"
+            f"Decodable plugin not recognized by dbt! Received output of `dbt --version`:\n{out.stderr}"
         )
 
 
